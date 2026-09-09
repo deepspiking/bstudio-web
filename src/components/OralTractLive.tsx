8b5b389bdbe5bfd6d070caa1944cc58f1e276966
deepspiking <deepspiking@users.noreply.github.com>
2026-09-09 10:29:28 +0900

    실시간 컨텍스트 창 300ms → 500ms

--- src/components/OralTractLive.tsx
+++ src/components/OralTractLive.tsx
@@ -532,7 +532,7 @@ export default function OralTractLive() {
       ws.send(
         JSON.stringify({
           encoder: 'ecapa',
-          windowSec: 0.3,
+          windowSec: 0.5,
           hopMs: HOP_MS,
           gateDbfs: EPD_DBFS,
           minWindowSpeech: 0.5,
